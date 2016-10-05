9bd850920b8d62827505700854116178ffe4b069
fabdin <fabdin@express.com>
2016-10-05 10:31:54 -0400

    correct code for the index.js

--- src/index.js
+++ src/index.js
@@ -1,197 +1,136 @@
-/**
-    Copyright 2014-2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 
-    Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
+var APP_ID = undefined; 
 
-        http://aws.amazon.com/apache2.0/
-
-    or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
-*/
-
-'use strict';
-
-function AlexaSkill(appId) {
-    this._appId = appId;
-}
-
-AlexaSkill.speechOutputType = {
-    PLAIN_TEXT: 'PlainText',
-    SSML: 'SSML'
-}
-
-AlexaSkill.prototype.requestHandlers = {
-    LaunchRequest: function (event, context, response) {
-        this.eventHandlers.onLaunch.call(this, event.request, event.session, response);
-    },
-
-    IntentRequest: function (event, context, response) {
-        this.eventHandlers.onIntent.call(this, event.request, event.session, response);
-    },
-
-    SessionEndedRequest: function (event, context) {
-        this.eventHandlers.onSessionEnded(event.request, event.session);
-        context.succeed();
-    }
-};
 
 /**
- * Override any of the eventHandlers as needed
+ * The AlexaSkill Module that has the AlexaSkill prototype and helper functions
  */
-AlexaSkill.prototype.eventHandlers = {
-    /**
-     * Called when the session starts.
-     * Subclasses could have overriden this function to open any necessary resources.
-     */
-    onSessionStarted: function (sessionStartedRequest, session) {
+var AlexaSkill = require('./AlexaSkill');
+
+var SmartShelf = function(){
+  AlexaSkill.call(this, APP_ID);  
+};
+
+
+SmartShelf.prototype = Object.create(AlexaSkill.prototype);
+SmartShelf.prototype.constructor = SmartShelf;
+
+SmartShelf.prototype.eventHandlers.onSessionStarted = function (sessionStartedRequest, session) {
+     console.log("SmartShelf onSessionStarted requestId: " + sessionStartedRequest.requestId
+        + ", sessionId: " + session.sessionId);
+    
+};
+
+SmartShelf.prototype.eventHandlers.onLaunch = function (launchRequest, session, response) {
+     console.log("SmartShelf onLaunch requestId: " + launchRequest.requestId + ", sessionId: " + session.sessionId);
+    getWelcomeResponse(response);
+};
+
+SmartShelf.prototype.eventHandlers.onSessionEnded = function (sessionEndedRequest, session) {
+    console.log("onSessionEnded requestId: " + sessionEndedRequest.requestId
+        + ", sessionId: " + session.sessionId);
+};
+
+
+SmartShelf.prototype.intentHandlers = {
+
+    "GetShelfScalesInfoIntent": function (intent, session, response) {
+        handleSmartShelf(intent, session, response);
     },
 
-    /**
-     * Called when the user invokes the skill without specifying what they want.
-     * The subclass must override this function and provide feedback to the user.
-     */
-    onLaunch: function (launchRequest, session, response) {
-        throw "onLaunch should be overriden by subclass";
+    "AMAZON.HelpIntent": function (intent, session, response) {
+        var speechText = "Help Intent";
+        
+        var speechOutput = {
+            speech: speechText,
+            type: AlexaSkill.speechOutputType.PLAIN_TEXT
+        };
+        
+        response.tell(speechOutput);
     },
 
-    /**
-     * Called when the user specifies an intent.
-     */
-    onIntent: function (intentRequest, session, response) {
-        var intent = intentRequest.intent,
-            intentName = intentRequest.intent.name,
-            intentHandler = this.intentHandlers[intentName];
-        if (intentHandler) {
-            console.log('dispatch intent = ' + intentName);
-            intentHandler.call(this, intent, session, response);
-        } else {
-            throw 'Unsupported intent = ' + intentName;
-        }
+    "AMAZON.StopIntent": function (intent, session, response) {
+        var speechOutput = {
+                speech: "Goodbye",
+                type: AlexaSkill.speechOutputType.PLAIN_TEXT
+        };
+        response.tell(speechOutput);
     },
 
-    /**
-     * Called when the user ends the session.
-     * Subclasses could have overriden this function to close any open resources.
-     */
-    onSessionEnded: function (sessionEndedRequest, session) {
+    "AMAZON.CancelIntent": function (intent, session, response) {
+        var speechOutput = {
+                speech: "Goodbye",
+                type: AlexaSkill.speechOutputType.PLAIN_TEXT
+        };
+        response.tell(speechOutput);
     }
 };
 
 /**
- * Subclasses should override the intentHandlers with the functions to handle specific intents.
+ * Function to handle the onLaunch skill behavior
  */
-AlexaSkill.prototype.intentHandlers = {};
 
-AlexaSkill.prototype.execute = function (event, context) {
-    try {
-        console.log("session applicationId: " + event.session.application.applicationId);
+function getWelcomeResponse(response) {
+    // If we wanted to initialize the session to have some attributes we could add those here.
+    var cardTitle = "Your Smart Shelf";
+    var repromptText = "With Smart Shelf , you can check the weight of your items and order a new items ";
+    var speechText = "<p>Smart Shelf.</p> <p>How can help you?</p>";
+    var cardOutput = "How can i help you?";
+    // If the user either does not reply to the welcome message or says something that is not
+    // understood, they will be prompted again with this text.
 
-        // Validate that this request originated from authorized source.
-        if (this._appId && event.session.application.applicationId !== this._appId) {
-            console.log("The applicationIds don't match : " + event.session.application.applicationId + " and "
-                + this._appId);
-            throw "Invalid applicationId";
-        }
-
-        if (!event.session.attributes) {
-            event.session.attributes = {};
-        }
-
-        if (event.session.new) {
-            this.eventHandlers.onSessionStarted(event.request, event.session);
-        }
-
-        // Route the request to the proper handler which may have been overriden.
-        var requestHandler = this.requestHandlers[event.request.type];
-        requestHandler.call(this, event, context, new Response(context, event.session));
-    } catch (e) {
-        console.log("Unexpected exception " + e);
-        context.fail(e);
-    }
-};
-
-var Response = function (context, session) {
-    this._context = context;
-    this._session = session;
-};
-
-function createSpeechObject(optionsParam) {
-    if (optionsParam && optionsParam.type === 'SSML') {
-        return {
-            type: optionsParam.type,
-            ssml: optionsParam.speech
-        };
-    } else {
-        return {
-            type: optionsParam.type || 'PlainText',
-            text: optionsParam.speech || optionsParam
-        }
-    }
+    var speechOutput = {
+        speech: "<speak>" + speechText + "</speak>",
+        type: AlexaSkill.speechOutputType.SSML
+    };
+    var repromptOutput = {
+        speech: repromptText,
+        type: AlexaSkill.speechOutputType.PLAIN_TEXT
+    };
+    response.askWithCard(speechOutput, repromptOutput, cardTitle, cardOutput);
 }
 
-Response.prototype = (function () {
-    var buildSpeechletResponse = function (options) {
-        var alexaResponse = {
-            outputSpeech: createSpeechObject(options.output),
-            shouldEndSession: options.shouldEndSession
-        };
-        if (options.reprompt) {
-            alexaResponse.reprompt = {
-                outputSpeech: createSpeechObject(options.reprompt)
-            };
-        }
-        if (options.cardTitle && options.cardContent) {
-            alexaResponse.card = {
-                type: "Simple",
-                title: options.cardTitle,
-                content: options.cardContent
-            };
-        }
-        var returnResult = {
-                version: '1.0',
-                response: alexaResponse
-        };
-        if (options.session && options.session.attributes) {
-            returnResult.sessionAttributes = options.session.attributes;
-        }
-        return returnResult;
-    };
+/**
+ * Gets a poster prepares the speech to reply to the user.
+ */
+function handleSmartShelf(intent, session, response) {
+    
+    getJSONSmartShelfInfo(null,null,function(data){
+        response.tell(data);
+    });    
+}
 
-    return {
-        tell: function (speechOutput) {
-            this._context.succeed(buildSpeechletResponse({
-                session: this._session,
-                output: speechOutput,
-                shouldEndSession: true
-            }));
-        },
-        tellWithCard: function (speechOutput, cardTitle, cardContent) {
-            this._context.succeed(buildSpeechletResponse({
-                session: this._session,
-                output: speechOutput,
-                cardTitle: cardTitle,
-                cardContent: cardContent,
-                shouldEndSession: true
-            }));
-        },
-        ask: function (speechOutput, repromptSpeech) {
-            this._context.succeed(buildSpeechletResponse({
-                session: this._session,
-                output: speechOutput,
-                reprompt: repromptSpeech,
-                shouldEndSession: false
-            }));
-        },
-        askWithCard: function (speechOutput, repromptSpeech, cardTitle, cardContent) {
-            this._context.succeed(buildSpeechletResponse({
-                session: this._session,
-                output: speechOutput,
-                reprompt: repromptSpeech,
-                cardTitle: cardTitle,
-                cardContent: cardContent,
-                shouldEndSession: false
-            }));
-        }
-    };
-})();
 
-module.exports = AlexaSkill;
+var urlPrefix = 'http://smartshelf.mybluemix.net/main/scaleInfo';
+var https = require('http');
+var fs = require('fs');
+
+function getJSONSmartShelfInfo(day, date, eventCallback) {
+    var url = urlPrefix;
+
+    https.get(url, function(res) {
+        var body = '';
+
+        res.on('data', function (chunk) {
+            body += chunk;
+        });
+
+        res.on('end', function () {
+            
+            console.log(body);
+            var obj=JSON.parse(body);
+            eventCallback(obj.product.name);
+        });
+    }).on('error', function (e) {
+        console.log("Got error: ", e);
+    });
+}
+
+
+// Create the handler that responds to the Alexa Request.
+exports.handler = function (event, context) {
+    
+    var skill = new SmartShelf();
+    skill.execute(event, context);
+};
+
